ce5bc02ac6f775593892bcd166422a0403e16e66
Студент <student@ad.anosov.ru>
2023-09-02 10:39:47 +0500

    Создан репозиторий

---
+++ lab1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Размер: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] ints = new int[size];
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                Console.WriteLine($"{i}: ");
+                ints[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
